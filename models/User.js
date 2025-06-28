9c42eb43cb1a537d57ccfe2b587c37f4f449b1a5
Nimra-Bilal <yoyo123000001@gmail.com>
2025-06-29 00:30:15 +0500

    user

--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const UserSchema = new mongoose.Schema(
     {
         _id: { type: String, required: true },
         name: { type: String, required: true },
-        email: { type: String, required: true, unique: true },
+        email: { type: String, required: true},
         image: { type: String, required: false },
         },
 
